82121ca90006ff7c1ebea9b7bed65444f1fc9f2a
NikitaGupta-17 <75840623+NikitaGupta-17@users.noreply.github.com>
2021-12-07 22:21:14 +0530

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,10 @@
+public class SumUdingCommandLineArg
+{  
+public static void main(String ar[])  
+{  
+int x = Integer.parseInt(ar[0]);
+int y = Integer.parseInt(ar[1]);  
+int sum = x + y;  
+System.out.println("The sum of x and y is: " +sum);  
+}  
+}
